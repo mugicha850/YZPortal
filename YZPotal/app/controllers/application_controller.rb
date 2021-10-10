183bf702f815b29675be7e0a34bb587e8d85c8cc
mugicha850 <tomo.fh.123@gmail.com>
2021-10-10 15:46:09 +0900

    コミット内容を修正

--- YZPotal/app/controllers/application_controller.rb
+++ YZPotal/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   layout :set_layout
 
     private def set_layout
-      if params[:controller].match(%r{¥A(staff|admin|customer)/})
+      if params[:controller].match(%r{\A(staff|admin|customer)/})
         Regexp.last_match[1]
       else
         "staff"
